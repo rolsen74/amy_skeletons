b8537dea47547c5cdeaad61bb9c1adc9192e74da
Rene W. Olsen <renewolsen@gmail.com>
2025-09-20 11:14:58 +0200

    Removed unused entries

--- lib_Docky/inc_private/Library.h
+++ lib_Docky/inc_private/Library.h
@@ -35,39 +35,7 @@ struct DockyData
 	U32								dd_Height;
 	struct RastPort *				dd_RenderRPort;
 	struct Screen *					dd_Screen;
-
-	// struct Library *	    	dd_SocketBase;
-	// struct SocketIFace *		dd_ISocket;
-
 	struct DockySize				dd_DockySize;
-
-	// int32						dd_RedrawDelay;
-
-	// struct TextAttr				dd_SkillFont;
-	// struct TextFont *			dd_SkillFontAdr;
-
-	// uint64						dd_ReadBytes;
-	// uint64						dd_SendBytes;
-
-	// uint32						dd_Total[ myDocky_MeterWidth ];
-	// uint64						dd_TotalMax;
-	// uint64						dd_OldTotal;
-	// uint32						dd_Diff;
-
-	// int32						dd_Bank;
-	// int32						dd_Index;
-	// int32						dd_Delay;
-	// CONST_STRPTR				dd_InterfaceName;
-
-	// uint32						dd_SmallGfx;
-	// uint32						dd_ShowCurrent;
-
-	// uint32 *					dd_WorkBuffer;
-
-	// uint64						dd_TimeBaseSpeed;
-	// uint64						dd_prev_total_time;
-	// uint64						dd_prev_idle_time;
-	// struct ETask *				dd_idle_etask;
 };
 
 // --
