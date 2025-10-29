626305adb372a21726f06414dfbe18bb82b0a0da
Rene W. Olsen <renewolsen@gmail.com>
2025-10-29 01:48:52 +0100

    Rename U32 to Tag

--- lib_Gadget/src/Library.c
+++ lib_Gadget/src/Library.c
@@ -46,10 +46,10 @@ static const PTR _main_Vectors[] =
 
 static const struct TagItem _main_Tags[] =
 {
-	{ MIT_Name,			(U32) "main"		},
-	{ MIT_VectorTable,	(U32) _main_Vectors	},
-	{ MIT_Version,		(U32) 1				},
-	{ TAG_END,			(U32) 0				}
+	{ MIT_Name,			(Tag) "main"		},
+	{ MIT_VectorTable,	(Tag) _main_Vectors	},
+	{ MIT_Version,		(Tag) 1				},
+	{ TAG_END,			(Tag) 0				}
 };
 
 // --
